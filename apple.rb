57e50600dfe45e51d541708483e07c1d9e56ce5d
EC2 Default User <ec2-user@ip-172-31-41-190.ap-northeast-1.compute.internal>
2023-01-16 12:37:31 +0000

    [Add]2-1~12

---
+++ apple.rb
@@ -0,0 +1,9 @@
+apple = "Yamanashi"
+
+if apple == "Aomori"
+  puts "このりんごは青森産です"
+elsif apple =="Nagano"
+  puts "このりんごは青森産ではなく、長野産です"
+else
+  puts "このりんごは青森産でも長野産でもありません"
+end
---
+++ constant.rb
@@ -0,0 +1,4 @@
+pi = 3.14
+puts pi
+pi = 100
+puts pi
